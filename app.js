735b1d6a0f9f95e9c7c421fcffc83457f3beea33
Jaydar Mitchell <jaydarmitchell@gmail.com>
2025-11-03 23:29:27 -0500

    Finally

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
+
     const button = document.getElementById("search_button");
+    const input = document.getElementById("search_input");
+    const resultDiv = document.getElementById("result");
 
     button.addEventListener("click", function() {
-        // Code that utilizes fetch function to Make AJAX request
-        fetch("superheroes.php")
-
-
-            .then(response => response.text()) // PHP outputs HTML
-
-
+        const query = encodeURIComponent(input.value.trim());
+// utilize fetch to search for heroes
+        fetch(`superheroes.php?query=${query}`)
+            .then(response => response.text())
             .then(data => {
-
-                const temp = document.createElement("div");
-                temp.innerHTML = data;
-
-
-                const hero_names = Array.from(temp.querySelectorAll("li"))
-                    .map(li => li.textContent);
-
-                //alert box with .join("\n") for spacing
-                alert(hero_names.join("\n"));
+                resultDiv.innerHTML = data; // Display the returned HTML in the div
             })
 
     });
 });
+
+//Java script code for styling:
+const logo = document.getElementById("avenger_logo");
+const logos = ["shield.png", "Spidey.png","panther.png", "HULK.png"];
+let current = 0;
+
+setInterval(() => {
+
+
+    logo.style.opacity = 0;
+
+    setTimeout(() => {
+
+        current = (current + 1) % logos.length;
+
+        logo.src = logos[current];
+
+
+        logo.style.opacity = 1;
+    }, 1000);
+}, 3000);
+
